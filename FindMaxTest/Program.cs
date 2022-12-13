11a16ede73706d690ae9479f1eb2e2646312f944
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-13 21:09:39 +0530

    [SaiVenkat]-Find The Maximum String

--- FindMaxTest/Program.cs
+++ FindMaxTest/Program.cs
@@ -5,14 +5,19 @@ namespace FindMaxTest
     {
         public static void Main(string[] args)
         {
-            int Maximum = FindMax.num(29, 43, 81);
+            int Maximum = FindMax.num(25, 43, 91);
 
-            float Fmax = FindMaxFloat.num(8.9f, 27.6f, 16.4f);
+            float Fmax = FindMaxFloat.num(81.9f, 27.6f, 16.4f);
+
+            string str = FindMaxString.fruit("Apple", "Peach", "Banana");
 
             Console.Write(Maximum);
             Console.WriteLine(" is the Maximum Number");
+
             Console.Write(Fmax);
-            Console.Write(" is the Maximum Float Number");
+            Console.WriteLine(" is the Maximum Float Number");
+
+            Console.WriteLine(str);
         }
     }
 }
