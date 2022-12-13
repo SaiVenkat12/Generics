263c5979243b98974644bf9cbc60cca14737c547
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-13 20:09:30 +0530

    [SaiVenkat]-Find The Maximum Integer

---
+++ FindMaxTest/FindMax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxTest
+{
+    public class FindMax
+    {
+        public static int num(int First, int SecondNo, int ThirdNo)
+        {
+            if (First.CompareTo(SecondNo) > 0 && First.CompareTo(ThirdNo) > 0)
+            {
+                return First;
+            }
+            if (SecondNo.CompareTo(First) > 0 && SecondNo.CompareTo(ThirdNo) > 0)
+            {
+                return SecondNo;
+            }
+
+            if (ThirdNo.CompareTo(First) > 0 && ThirdNo.CompareTo(SecondNo) > 0)
+            {
+                return ThirdNo;
+            }
+            throw new Exception("First, Second and Third Numbers are same");
+        }
+    }
+}
---
+++ FindMaxTest/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace FindMaxTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int Maximum = FindMax.num(29, 43, 81);
+
+            Console.Write(Maximum);
+            Console.Write(" is the Maximum Number");
+        }
+    }
+}
